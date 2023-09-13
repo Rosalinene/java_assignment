2e0b65d138727bb2985a3dacc2de84969be73db3
Rosaline <huong22121804@gmail.com>
2023-09-13 18:06:31 -0400

    getting rid of setget()

--- Inheritance/src/Electronics/Electronics.java
+++ Inheritance/src/Electronics/Electronics.java
@@ -26,35 +26,5 @@ public class Electronics
         //msg+= super.toString();
         
         return msg;
-    }//End 
-
-    public String getManufacturer() 
-    {
-        return manufacturer;
-    }//End getManufacturer
-
-    public void setManufacturer(String manufacturer) 
-    {
-        this.manufacturer = manufacturer;
-    }//End setManufacturer
-
-    public double getPrice() 
-    {
-        return price;
-    }//End getPrice
-
-    public void setPrice(double price) 
-    {
-        this.price = price;
-    }//End setPrice
-
-    public double getWeight() 
-    {
-        return weight;
-    }//End getWeight
-
-    public void setWeight(double weight) 
-    {
-        this.weight = weight;
-    }//End setWeight
+    }//End toString()
 }//End Electronics
